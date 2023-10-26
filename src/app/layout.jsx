5cc733d93bd146078fde307e17ac21a3a9b708fd
William Lowrimore <wlowrimore@gmail.com>
2023-10-25 19:31:22 -0500

    initial setup

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`{inter.className} container flex flex-col mx-auto`}>{children}</body>
     </html>
   )
 }
---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className='container flex flex-col justify-center items-center'>
+      <h1 className='text-4xl text-center text-rose-600'>App &gt; Page.jsx</h1>
+    </main>
+  )
+}
